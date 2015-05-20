989f699aec51363fd5d15408b17b7b6dedab0445
iGuest <dss@def.com>
2015-05-19 22:34:29 -0700

    dsd

--- app/src/main/java/edu/washington/cederw/arewethereyet/MyReceiver.java
+++ app/src/main/java/edu/washington/cederw/arewethereyet/MyReceiver.java
@@ -3,11 +3,14 @@ package edu.washington.cederw.arewethereyet;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.telephony.SmsManager;
 import android.util.Log;
 import android.widget.Toast;
 
 public class MyReceiver extends BroadcastReceiver {
+    private static SmsManager manager;
     public MyReceiver() {
+        manager =  SmsManager.getDefault();
     }
 
     @Override
@@ -15,10 +18,11 @@ public class MyReceiver extends BroadcastReceiver {
         // TODO: This method is called when the BroadcastReceiver is receiving
         // an Intent broadcast.
         Log.i("uhh", "got it");
-
         String number = intent.getStringExtra("num");
         String message = intent.getStringExtra("msg");
-        Toast.makeText(context, number + ": " + message, Toast.LENGTH_LONG).show();
+        manager.sendTextMessage(number,null,message, null,null);
+
+       // Toast.makeText(context, number + ": " + message, Toast.LENGTH_LONG).show();
 
     }
 }
